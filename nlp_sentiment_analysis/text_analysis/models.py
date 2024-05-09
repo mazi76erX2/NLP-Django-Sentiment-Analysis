8b27c473574346e6fedfa6ba515217ab7f41b416
Xolani <mazi76erx@gmail.com>
2024-05-09 17:26:48 +0200

    Added models, formatting and linting files
    Disabled docstring for modules and unscriptable.
    Added a Analysis model

--- nlp_sentiment_analysis/text_analysis/models.py
+++ nlp_sentiment_analysis/text_analysis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Analysis(models.Model):
+    """
+    This model stores the results of text sentiment analysis.
+
+    Fields:
+        text (str): The analyzed text.
+        polarity (Optional[float]):
+            The polarity score (-1.0 for negative, 1.0 for positive).
+        subjectivity (Optional[float]):
+            The subjectivity score (0.0 for objective, 1.0 for subjective).
+        created_at (models.DateTimeField):
+            The timestamp when the analysis was created. Automatically set on creation.
+    """
+
+    text = models.TextField(blank=False, null=False)
+    polarity = models.FloatField(blank=True, null=True)
+    subjectivity = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.text[:20]}..." if self.text is not None else ""
